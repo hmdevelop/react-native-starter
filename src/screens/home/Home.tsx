7d1d0691793d40a4049d9f915859bc0d3ac4f2f8
Mustafa Topal2 <cams@Mustafa-MacBook-Pro.local>
2019-02-17 14:02:15 +0300

    Rigel-Native-Master

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,27 +1,26 @@
-import * as React from 'react';
-import { View, Text, Image } from 'react-native';
-import { observer } from 'mobx-react';
-import { Navigation } from 'react-native-navigation';
-import CodePush from 'react-native-code-push';
+import * as React from "react";
+import { View, Text, Image } from "react-native";
+import { observer } from "mobx-react";
+import { Navigation } from "react-native-navigation";
+import CodePush from "react-native-code-push";
 
-import { UIStore } from 'stores/UIStore';
-import { codePushConfig } from 'utils/code-push';
-import { COUNTER, IScreen } from 'screens';
-import { Button } from 'components/button/Button';
+import { UIStore } from "stores/UIStore";
+import { codePushConfig } from "utils/code-push";
+import { COUNTER, IScreen } from "screens";
+import { Button } from "components/button/Button";
 
-const s = require('./Home.scss');
+const s = require("./Home.scss");
 
 @CodePush(codePushConfig())
 @observer
 export class Home extends React.Component<IScreen> {
-
   static get options() {
     return {
       topBar: {
         title: {
-          text: 'Home',
-        },
-      },
+          text: "Home"
+        }
+      }
     };
   }
 
@@ -32,10 +31,10 @@ export class Home extends React.Component<IScreen> {
   onCounterScreenPress = () => {
     Navigation.push(this.props.componentId, {
       component: {
-        name: COUNTER,
-      },
+        name: COUNTER
+      }
     });
-  }
+  };
 
   render() {
     return (
@@ -43,11 +42,11 @@ export class Home extends React.Component<IScreen> {
         <View style={s.content}>
           <Image
             style={s.logo}
-            source={require('assets/images/logo.png')}
+            source={require("assets/images/logo.png")}
             resizeMode="contain"
           />
 
-          <Text style={s.text}>Welcome Home</Text>
+          <Text style={s.text}>Welcome Home Rigel-Native-Master</Text>
         </View>
 
         <Button onPress={this.onCounterScreenPress} title="Counter Screen" />
