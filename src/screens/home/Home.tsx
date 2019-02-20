6eab19bd9aa65d4c12cee7a66edc8c024d04f8a0
mustafa topal <mustafatopal2@gmail.com>
2019-02-21 01:38:34 +0300

    gamma done

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -8,11 +8,6 @@ import { UIStore } from "stores/UIStore";
 import { codePushConfig } from "utils/code-push";
 import { COUNTER, IScreen } from "screens";
 import { Button } from "components/button/Button";
-// @ts-ignore
-import ImagePicker from "../../components/examples/ImagePicker.js";
-// @ts-ignore
-import Icon from "react-native-vector-icons/FontAwesome5";
-import Video from "react-native-video";
 
 const s = require("./Home.scss");
 
@@ -48,26 +43,11 @@ export class Home extends React.Component<IScreen> {
   render() {
     return (
       <View style={s.host} testID="HOME_SCREEN">
-        <Icon name="comments" size={30} color="#900" />
         <View style={s.content}>
-          <Text style={s.text}>Welcome Home Rigel-Native-Beta</Text>
+          <Text style={s.text}>Welcome Home Rigel-Native-Gamma</Text>
         </View>
 
         <Button onPress={this.onCounterScreenPress} title="Counter Screen" />
-        <ImagePicker />
-        <Video
-          source={{
-            uri:
-              "https://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_10mb.mp4"
-          }} // Can be a URL or a local file.
-          // @ts-ignore
-          ref={ref => {
-            // @ts-ignore
-            this.player = ref;
-          }} // Store reference
-          onError={this.videoError} // Callback when video cannot be loaded
-          style={{ position: "absolute", top: 0, left: 0, bottom: 0, right: 0 }}
-        />
       </View>
     );
   }
