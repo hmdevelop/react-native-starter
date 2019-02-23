1e39bc4636e885ed1b508efceeb5b6b7a88b2101
mustafa topal <mustafatopal2@gmail.com>
2019-02-24 01:22:57 +0300

    react-native-blur ios

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import { observer } from "mobx-react";
 import { Navigation } from "react-native-navigation";
 import CodePush from "react-native-code-push";
@@ -8,6 +8,7 @@ import { UIStore } from "stores/UIStore";
 import { codePushConfig } from "utils/code-push";
 import { COUNTER, IScreen } from "screens";
 import { Button } from "components/button/Button";
+import { BlurView } from "react-native-blur";
 
 const s = require("./Home.scss");
 
@@ -23,6 +24,7 @@ export class Home extends React.Component<IScreen> {
       }
     };
   }
+  state = { viewRef: null };
 
   componentDidAppear() {
     UIStore.setComponentId(this.props.componentId);
@@ -44,7 +46,27 @@ export class Home extends React.Component<IScreen> {
         </View>
 
         <Button onPress={this.onCounterScreenPress} title="Counter Screen" />
+        <BlurView
+          style={styles.absolute}
+          viewRef={this.state.viewRef}
+          blurType="light"
+          blurAmount={10}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  absolute: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0
+  }
+});
