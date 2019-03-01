13777b275dd50fa4fcdfb71429a8750c565d56c2
mustafa topal <mustafatopal2@gmail.com>
2019-03-01 23:38:58 +0300

    ....

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,6 +1,13 @@
 import { StyleSheet, Dimensions } from "react-native";
-import { setVar, setTheme, setThemeVars } from "react-native-ueno-css-modules";
+import {
+  setVar,
+  setTheme,
+  setThemeVars,
+  setVars
+} from "react-native-ueno-css-modules";
 import { Navigation } from "react-native-navigation";
+// @ts-ignore
+import { normalize } from "../helpers/normalize.js";
 
 // Add some static re-usable css variables here
 setVar("--hairline-width", StyleSheet.hairlineWidth);
@@ -23,7 +30,28 @@ export async function updateTheme() {
   setVar("--bottom-tabs-height", res.bottomTabsHeight);
   setVar("--status-bar-height", res.statusBarHeight);
   setVar("--top-bar-height", res.topBarHeight);
-  setVar("--head-font", 40);
+
+  setVars({
+    r1: normalize(1),
+    r2: normalize(2),
+    r3: normalize(3),
+    r4: normalize(4),
+    r5: normalize(5),
+    r6: normalize(6),
+    r7: normalize(7),
+    r8: normalize(8),
+    r9: normalize(9),
+    r10: normalize(10),
+    r12: normalize(12),
+    r14: normalize(14),
+    r16: normalize(16),
+    r20: normalize(20),
+    r24: normalize(24),
+    r34: normalize(34),
+    r48: normalize(48),
+    r60: normalize(60),
+    r96: normalize(96)
+  });
   // Add dynamic variables here
   // ...
 
