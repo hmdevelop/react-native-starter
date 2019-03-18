dfa6d0baadc7fddb26cb4232b23642dd2384eefc
mustafa topal <mustafatopal2@gmail.com>
2019-03-19 01:51:46 +0300

    card comp static styling done

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -51,22 +51,63 @@ export class Card extends React.PureComponent<IProps> {
 
     return (
       <View style={s.container}>
-        <View style={s.c}>
+        <View>
           <Image
             style={s.cardImage}
             source={require('../../assets/postImg.png')}
           />
+
           <View style={s.cardImageFooter}>
             <Image
               source={require('../../assets/User.png')}
               style={s.cardImageUser}
             />
+
             <View style={s.cardImageUserName}>
               <Text style={s.cardImageUserNameText}>Pearl Freeman</Text>
+              <Text style={s.cardImageTimeAgo}>2 hours ago</Text>
+            </View>
+          </View>
+        </View>
+        <View>
+          <View style={s.bodyTop}>
+            <Text style={s.bodyTopText}>#relax, #travel</Text>
+          </View>
+          <View style={s.bodyMid}>
+            <Text style={s.bodyMidText}>
+              Coventry is a city with a thousand years of history that has
+              plenty to offer the visiting tourist. Located in the heart of
+              Warwickshire, which is well-known as Shakespeare’s county.
+            </Text>
+          </View>
+          <View style={s.bodyFooter}>
+            <View style={s.bodyFooterLeft}>
+              <Image source={require('../../assets/Like.png')} />
+              <Text style={s.bodyFooterLeftText}>1125</Text>
+              <Image
+                style={{ marginLeft: 10 }}
+                source={require('../../assets/Comments.png')}
+              />
+              <Text style={s.bodyFooterLeftText}>1125</Text>
+            </View>
+            <View style={s.bodyFooterRight}>
+              <Image
+                style={{ marginLeft: 10 }}
+                source={require('../../assets/Share.png')}
+              />
             </View>
           </View>
         </View>
       </View>
     );
   }
+}
+{
+  /* <View style={s.cardBody}>
+<Text style={s.cardBodyText}>
+  Coventry is a city with a thousand years of history that has
+  plenty to offer the visiting tourist. Located in the heart of
+  Warwickshire, which is well-known as Shakespeare’s county.
+</Text>
+</View> */
 }
